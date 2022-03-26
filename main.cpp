8cceac71cd7f5ccc65b3b7e922b139c14acb90fc
Shalitha Suranga <shalithasuranga@gmail.com>
2022-03-26 19:17:22 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main() {
     cout << "5 + 10 = " << add::calc(5, 10) << endl;
     cout << "15 - 10 = " << subtract::calc(15, 10) << endl;
+    
     #if defined(BZ_TESTV)
     cout << "BZ_TESTV is defined" << endl;
     #endif
